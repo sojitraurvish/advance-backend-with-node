d8faeb1b8579d145fd12294de8599d2506277b58
urvish <sojitraurvish0@gmail.com>
2025-02-26 22:20:43 +0530

    complet first part of aws

--- node 4 aws/n1.js
+++ node 4 aws/n1.js
@@ -82,7 +82,88 @@
 
 // so not by running above command i am connected to my server
 
-//  now i will clone my backend code on server by git clone <uri>,some time when you start server you do not have internet access how do i connect to the internet but how do i know so i got these errors(see phto 4) so how to fix this is open this file and add this entry over there (see photo 5,6,7) and you have to add that entry for both normal and super user (see phto 8) and add that entry after that you will be able to access internet form your aws server and you will be able to clone your repo, once it get cloned  
+//  now i will clone my backend code on server by git clone <uri>,some time when you start server you do not have internet access how do i connect to the internet but how do i know so i got these errors(see phto 4) so how to fix this is open this file and add this entry over there (see photo 5,6,7) and you have to add that entry for both normal and super user (see phto 8) and add that entry after that you will be able to access internet form your aws server and you will be able to clone your repo, once it get cloned
+
+// if you stop your server instance and start it again your public ip get change
+
+// now i have clone my code on server now i have to npm i but before that i have to install node i will install via nvm node version manager and via this command(ptho 9) i can install it after intallation how to use it in terminal(phto 10). then install node via nvm but this is the ideal way no because docker container need node js not your machine need node js, in machine we first create docker container and inside that we install node js so each docker container run seprately and each can have there own diff app runnin in some i might install laravel also or java also but my machine does have that so that on single machine i can run diff applications
+
+// but right now we are not useing docker so instll here in machine only with nvm i 20@ to install node once node get install then run node index.js
+
+// now app is running on my server it mean my server has someting runing on port 8080, so if i go to ip:port i won't be able to access it because my security group says only 443,80, and 22 is open nothing else is open so i wont be able to access it on 8080, so let's open port 8080, in our index.js file we mention port 8080 so there some process that listen on port 8080, now on aws dashbord click on istance and slect securiy option click on that specific sucurity rule (phto 11) click on inbound runle select edit inbound ruldes , click on add rules add port 8080 for anywhere for ipv4 and ipv6 so add two rules ipv4 and ipv6 and check now it is working ON port 8080 but still you have to write port 8080 which is very ugly will see how to fix that but right now our app is running that good 
+
+// to learn kubernatis a guy call netwark chunk
+
+// i want http request open for port 8080 and what does http use under the hood it uses tcp and i want custome port 8080 open on tcp and that way it called custom tcp 
+// then can not you directly do app.listen(80) no you can not do that because your node process does not have access to use port 80 though you can do it by enabling some setting 
+//  but now our application is runing is on port 8080
+
+
+// now we learn about reverse proxy
+
+// so now we have aws server with open port 80,443,22 and there is one node.js process which is running on port 8080 but when i try to access this via my ip or domain(urivhs.com:8080) then i have to add port there and it is very ugly so is there any way to do like rather then it is running on port 8080 it run on port 80 so that i do not have to mention port and i do not mention it got redirect 80 by default which http. 
+
+// now you are unable to do that your node process run on port 80 but let's say you are able to do that now you have started first process on port 80 and now you want to start second process , you can have two confilcting process on port 80, let's say you brought one server and you have to node process and your public ip is 1.2.3.4 and you want that dev.urvish1.com refer to first node proceess and dev.urvish2.com points to secode node process. and this both domain should refere to your server how to do that how will you run two different processes on same port which is why it make sance to use reverce proxy 
+// 
+// if this is my ec2 machine and suppose one node process runing on port 8080 and anther is on port 8081 what i can do is on port 80 i am ruinng someing else which is usallly called reverce proxy and this can decide if the incomming url is urvish1.100x.com then send the request to first process which is running on port 8080 if it is urvish2.100x.com then send it to second process which is runing on port 8081 why is this useful because on same server i can start multiple node js process all running on diffrent ports (see phto 12) that what reverce proxy mean one seperate process is running on port 80 and that will decide which process your request want to access see phto 13 so this is revece porcy and this do opposite to proxy then what does proxy do
+
+// some time in clollage some bad website are not accesable but by vpn or proxy or forward proxy you can access it so your collage blocked some website so when you request bad.com it got to proxy mean vpn and vpn fetches that site and return you that is proxy mean now we learn proxy and revercy proxy
+
+// now let see how to emit port and get normal url and set up reverce proxy on port 80 and it will forword req to diff process based to domain it get form req 
+
+// whant it nginx is open sorce(see photo 14) for lot of thing and one of the use case is set reverce proxy
+
+// (see phto 15)
+
+// first install nginx in your instance
+// cmd sudo apt update
+// cmd sudo apt install nginx
+
+// once it get install go to ec2 url and someting get open which is welcome to nginx on defalt url which is on port 80,so you will be able to see nginx page on your server  on port 80 which is not your node js process which is someting else and we want to tell nginx you are reverce proxy please proxy my request we have not told anything that why you see welcome to nginx, but now on port 80 nginx is runing now we need to forword the requst on diff ports 
+
+// copy your server instance public ip to setup domain (see phto 16) i have addeed two entries which point same server ip, now one thing is ramain to tell nginx to create the revece proxy and for that we have to create nginx configration file
+
+// write this commad sudo vi /etc/nginx/nginx.conf this is defalt enginx config file, by defalut what runs on port 80 metioned here what are we see riging now we are see welcome neginx mess so this why you can see because the configaration is present in /etc/nginx/nginx.conf if you open it you see this(phto 17) and (welecme file that we are seeing) there configaration also present here in this nginx.conf file, so it also serve html file(by defalut) not only reverce proxy, but we want that config file to behave like reverce proxy and what you put there (see pic 18) see configarations
+
+// everts you can completey ignore right nwo
+
+// http { //start an http server 
+//  server{
+//  listen 80; //listening on port 80
+//  server_name  urvish1.100x.com // which takes this domain
+
+// location /{ 
+//   proxy_pass http://localhost:8080 //and proxy it to 8080
+
+// just this you need to understand, so now  copy this phto 18 configaration, delete old file content in nginx.conf file with cmd sudo rm /etc/nginx/nginx.conf and reopen file with cmd sudo vi /etc/nginx/nginx.conf and past content you copy and save and reload it with this cmd sudo nginx -s reload this is the commad to restart nginx whenever you change the cofigaration of nginx   now hit your domain and it will start wokring
+
+// small hack
+// when i ping urvish1.100x.com in my terminal with cmd ping urvish1.100x.com it shows me it's ip but in my local i can change that now urvish1.100x.com points to this diff ip but in my local only with this cmd sudo vi /etc/hosts and passwod and it open one file and there and your domain and ip once you save that file your that domin pints to that ip which you have added(check photo 18) and check it by cmd ping domain
+// for second configaration copy above confie exactly bello and change diff domain and it will work
+// you can do this thing also with docker see phto 20
+// or for both process create seperate docker instance and also one for nginx how ever you want to make combination you can do whichever is sutable for you
+
+// if you exit from server and still you wnat taht your node process is runing then use pm2
+
+// mange DNS section you can change dns entry and with diff ip
+
+// certificat management that we will see on next video mean how to use our domain with https instead of http
+
+// nginx also used as load balcncer 
+// you can also access your server form aws window
+
+
+
+
+
+
+
+
+
+
+
+ 
+
 
 
 
