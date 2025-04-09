9001916033b76b6b80c2da51c2a98453ea31d4e4
urvish <sojitraurvish0@gmail.com>
2025-04-09 21:12:38 +0530

    new changes

---
+++ node 14 pubsub queues and redis/n1.js
@@ -0,0 +1,6 @@
+
+// websockets are not that much used for backend to backend communication they are really user for forntend to backend communication
+
+// and these to are mostly use for backend to backend communication, it will help two diff backend to each other let dive deep in pubsub and messaging queues  
+
+// today we will dive deep in leet code example
